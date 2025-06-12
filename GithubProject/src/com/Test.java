f196eda42476f3793056ea5313ab2fc61b708779
Mahesh <maheshodnala5050@gmail.com>
2025-06-12 17:52:30 +0530

    Bye Feature Developed

--- GithubProject/src/com/Test.java
+++ GithubProject/src/com/Test.java
@@ -7,6 +7,10 @@ public class Test {
 		System.out.println("I am manager");
 	}
 	
+	public static void sayBye() {
+		System.out.println("Bye Feature by Developer 2");
+	}
+	
 	public static void sayHi() {
 		System.out.println("Hi Feature by Developer 1");
 	}
